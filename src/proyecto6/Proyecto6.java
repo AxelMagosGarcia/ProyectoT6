365670b3d813a96fd57eb11a55fa41a1f77f99e1
AXEL13S <94258253+AXEL13S@users.noreply.github.com>
2021-12-12 16:09:35 -0600

    HOLA MUNDO

--- src/proyecto6/Proyecto6.java
+++ src/proyecto6/Proyecto6.java
@@ -15,7 +15,7 @@ public class Proyecto6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola Jess, Bryan y Sebas");
     }
     
 }
