34f1d5f6de362feab4930473435f474a183eb7ae
Jesszagmer12 <94260154+Jesszagmer12@users.noreply.github.com>
2021-12-13 16:29:51 -0600

    Update Proyecto6.java

--- src/proyecto6/Proyecto6.java
+++ src/proyecto6/Proyecto6.java
@@ -15,7 +15,8 @@ public class Proyecto6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Jess, Bryan y Sebas");
+        System.out.println("que uvas");
+        
     }
     
 }
